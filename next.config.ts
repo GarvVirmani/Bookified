733a078cb44b08282b9e1d3630cef9d32ebb3b0d
GarvVirmani <garvirmani77@gmail.com>
2026-03-27 14:30:13 +0530

    confix fix

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,7 @@ const nextConfig: NextConfig = {
     },
     images: { remotePatterns: [
             { protocol: 'https', hostname: 'covers.openlibrary.org' },
-            { protocol: 'https', hostname: 'lspfdyhgsrgsxcju.public.blob.vercel-storage.com' },
+            { protocol: 'https', hostname: 'mxg0qfx3gitmmz39.public.blob.vercel-storage.com' },
         ]}
 };
 
